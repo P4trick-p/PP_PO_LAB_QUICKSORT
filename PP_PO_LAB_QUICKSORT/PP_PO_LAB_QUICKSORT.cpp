f7e00c4062a0424edd41f61d515ee860358e0b61
P4trick <3527184+P4trix@users.noreply.github.com>
2018-03-15 16:49:16 +0100

    Update PP_PO_LAB_QUICKSORT.cpp

--- PP_PO_LAB_QUICKSORT/PP_PO_LAB_QUICKSORT.cpp
+++ PP_PO_LAB_QUICKSORT/PP_PO_LAB_QUICKSORT.cpp
@@ -1,6 +1,3 @@
-
-
-#include "stdafx.h"
 #include <iostream>
 #include <time.h>
 #include <windows.h>
@@ -12,16 +9,22 @@
 
 using namespace std;
 
+int n;
+
 double t, start, stop;
-void quicksort(int tab[], int l, int r);
+void QuickSort(int tab[], int l, int r);
+void QuickSortIterative(int* liczba, int l, int r);
+int Partition(int* liczba, int left, int right);
 
 
 int main()
 {
-	
+
+
+	/*
 	string input_str;
 	vector<int> vec;
-	
+
 	getline(cin, input_str);
 
 	stringstream ss(input_str);
@@ -38,19 +41,32 @@ int main()
 	cout << endl;
 
 	int *tab;
-	tab = new int[i+2];
+	tab = new int[(i];
 
-	for (int j = 0; j < i + 2; j++)
+	for (int j = 0; j <= i; j++)
 	{
-		tab[j] = vec[j];
+	tab[j] = vec[j];
 	}
+	*/
 
-	system("pause");
 
-	cout << endl << "Przed sortowaniem:" << endl;
-	for (int j = 0; j < i+2; j++)
+	int i;
+	cout << "How many elements do you want to sort: ";
+	cin >> n;
+	int *tab;
+	tab = new int[n];
+	
+	srand(time(NULL));
+	for (int i = 0; i < n; i++)
 	{
-		if (j<i + 1)
+		tab[i] = rand() % 100;
+	}
+	system("pause");
+	
+	cout << endl << "Przed sortowaniem:" << endl;
+	for (int j = 0; j < n; j++)
+	{
+		if (j<n-1)
 		{
 			cout << tab[j] << ", ";
 		}
@@ -61,20 +77,54 @@ int main()
 	}
 	system("pause");
 
-	cout <<endl<<endl<< "Sorting data using quicksort. Please wait!" << endl;
+	int option;
+	cout << endl << endl << "1.Recursive" << endl << "2.Iterative" << endl << "Choose quicksort method: ";
+	cin >> option;
+	switch (option)
+	{
+	case 1:
+		cout << endl << endl << "Sorting data using quicksort. Please wait!" << endl;
+		start = clock();
+		QuickSort(tab, 0, n + 1);
+		stop = clock();
+		t = (double)(stop - start) / (CLOCKS_PER_SEC / 1000);
+		cout << endl << "Time needed to sort with quicksort: " << t << " s" << endl;
+		Sleep(1000);
+		break;
+	case 2:
+		cout << endl << endl << "Sorting data using quicksort. Please wait!" << endl;
+		start = clock();
+		QuickSortIterative(tab, 0, n-1);
+		stop = clock();
+		t = (double)(stop - start) / (CLOCKS_PER_SEC / 1000);
+		cout << endl << "Time needed to sort with quicksort: " << t << " s" << endl;
+		Sleep(1000);
+
+		break;
+	default:
+		cout << "Error";
+		exit(0);
+		break;
+	}
+
+
+	/*
+	cout << endl << endl << "Sorting data using quicksort. Please wait!" << endl;
 	start = clock();
-	quicksort(tab, 0, i + 1);
+	QuickSort(tab, 0, n-1);
 	stop = clock();
 	t = (double)(stop - start) / (CLOCKS_PER_SEC / 1000);
 	cout << endl << "Time needed to sort with quicksort: " << t << " s" << endl;
 	Sleep(1000);
+	*/
+
 
 	//// SPRAWDZENIE TABLICY ////
 	cout << "Sprawdzenie tablicy:" << endl;
-	for (int j = 0; j < i+2; j++)
+	for (int j = 0; j < n; j++)
 	{
 
-		if (j < i + 1)
+		if (j < n - 1)
 		{
 			cout << tab[j] << "," << " ";
 		}
@@ -87,62 +137,70 @@ int main()
 	system("pause");
 	return 0;
 
+
+
 	/*////////////////////////////////////////////////////////
+	cout << "How many elements do you want to sort?: ";
+	cin >> n;
+
+
 	int opt;
 	cout << "How many elements do you want to sort?: ";
 	cin >> n;
-	
+
 	cout << "MENU" << endl;
 	cout << "1.MANUAL INPUT" << endl;
 	cout << "2.RANDOM INPUT" << endl;
 	cout << "Choose option: ";
 	cin >> opt;
 	system("cls");
-
 	if (opt==1)
 	{
-		///////////		MANUAL	INPUT	///////////
-		...
-		..
-		.
+	///////////		MANUAL	INPUT	///////////
+	...
+	..
+	.
 	}
 	if (opt==2)
 	{
-	
-		///////////		RANDOM	INPUT	///////////
-		
-		srand(time(NULL));
-		for (int i = 0; i < n; i++)
-		{
-			liczba = rand() % 100;
-			tab[i] = liczba;
-		}
-	
+
+	///////////		RANDOM	INPUT	///////////
+
+	srand(time(NULL));
+	for (int i = 0; i < n; i++)
+	{
+	liczba = rand() % 100;
+	tab[i] = liczba;
 	}
-	
+
+	}
+
 	//// SPRAWDZENIE TABLICY ////
 	cout << "Sprawdzenie tablicy" << endl;
 	for (int i = 0; i < n; i++)
 	{
-		
-		if (i < n - 1)
-		{
-			cout << tab[i] << "," << " ";
-		}
-		else
-		{
-			cout << tab[i];
-		}
+
+	if (i < n - 1)
+	{
+	cout << tab[i] << "," << " ";
+	}
+	else
+	{
+	cout << tab[i];
+	}
 	}
 	cout << endl;
 	system("pause");
 	return 0;
 	*/////////////////////////////////////////////////////////
+
+
+	delete tab;
 }
 
 
 
-void quicksort(int *tab, int l, int r)
+void QuickSort(int *tab, int l, int r)
 {
 	int t = tab[(l + r) / 2];
 	int i, j, x;
@@ -158,8 +216,57 @@ void quicksort(int *tab, int l, int r)
 			i++;
 			j--;
 		}
-	} 
-	while (i <= j);
-	if (j > l) quicksort(tab, l, j);
-	if (i < r) quicksort(tab, i, r);
+	} while (i <= j);
+	if (j > l) QuickSort(tab, l, j);
+	if (i < r) QuickSort(tab, i, r);
+}
+
+int Partition(int* liczba, int left, int right) {
+	int x = liczba[right];
+	int i = (left - 1);
+
+	for (int j = left; j <= right - 1; ++j)
+	{
+		if (liczba[j] <= x)
+		{
+			++i;
+			swap(liczba[i], liczba[j]);
+		}
+	}
+
+	swap(liczba[i + 1], liczba[right]);
+
+	return (i + 1);
+}
+
+void QuickSortIterative(int* liczba, int l, int r) {
+	int i = l;
+	int j = r - 1;
+	int top = -1;
+	int* stack = new int[r];
+
+	stack[++top] = i;
+	stack[++top] = j;
+
+	while (top >= 0)
+	{
+		j = stack[top--];
+		i = stack[top--];
+
+		int p = Partition(liczba, i, j);
+
+		if (p - 1 > i)
+		{
+			stack[++top] = i;
+			stack[++top] = p - 1;
+		}
+
+		if (p + 1 < j)
+		{
+			stack[++top] = p + 1;
+			stack[++top] = j;
+		}
+	}
+
+	delete stack;
 }
